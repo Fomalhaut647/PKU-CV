8a156ee9b5e91e1a4115c2735ff5cd4522dc942f
Fomalhaut <fomalhaut@stu.pku.edu.cn>
2025-11-27 15:04:19 +0800

    format

--- Problem_Set_4/main.py
+++ Problem_Set_4/main.py
@@ -10,93 +10,102 @@ from typing import Tuple
 
 
 def normalize_disparity_map(disparity_map):
-    '''Normalize disparity map for visualization 
+    """Normalize disparity map for visualization
     disparity should be larger than zero
-    '''
+    """
     return np.maximum(disparity_map, 0.0) / (disparity_map.max() + 1e-10)
 
 
 def visualize_disparity_map(disparity_map, gt_map, save_path=None):
-    '''Visualize or save disparity map and compare with ground truth
-    '''
+    """Visualize or save disparity map and compare with ground truth"""
     # Normalize disparity maps
     disparity_map = normalize_disparity_map(disparity_map)
     gt_map = normalize_disparity_map(gt_map)
     # Visualize or save to file
     if save_path is None:
         concat_map = np.concatenate([disparity_map, gt_map], axis=1)
-        plt.imshow(concat_map, 'gray')
+        plt.imshow(concat_map, "gray")
         plt.show()
     else:
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         concat_map = np.concatenate([disparity_map, gt_map], axis=1)
-        plt.imsave(save_path, concat_map, cmap='gray')
+        plt.imsave(save_path, concat_map, cmap="gray")
 
 
 def task1_compute_disparity_map_simple(
-    ref_img: np.ndarray,        # shape (H, W)
-    sec_img: np.ndarray,        # shape (H, W)
-    window_size: int, 
-    disparity_range: Tuple[int, int],   # (min_disparity, max_disparity)
-    matching_function: str      # can be 'SSD', 'SAD', 'normalized_correlation'
+    ref_img: np.ndarray,  # shape (H, W)
+    sec_img: np.ndarray,  # shape (H, W)
+    window_size: int,
+    disparity_range: Tuple[int, int],  # (min_disparity, max_disparity)
+    matching_function: str,  # can be 'SSD', 'SAD', 'normalized_correlation'
 ):
-    '''Assume image planes are parallel to each other
+    """Assume image planes are parallel to each other
     Compute disparity map using simple stereo system following the steps:
     1. For each row, scan all pixels in that row
     2. Generate a window for each pixel in ref_img
-    3. Search for a disparity (d) within (min_disparity, max_disparity) in sec_img 
+    3. Search for a disparity (d) within (min_disparity, max_disparity) in sec_img
     4. Select the best disparity that minimize window difference between ref_img[row, col] and sec_img[row, col - d]
-    '''
+    """
 
     disparity_map = ...
-    
+
     return disparity_map
 
-def task1_simple_disparity(ref_img, sec_img, gt_map, img_name='tsukuba'):
-    '''Compute disparity maps for different settings
-    '''
+
+def task1_simple_disparity(ref_img, sec_img, gt_map, img_name="tsukuba"):
+    """Compute disparity maps for different settings"""
     window_sizes = [...]  # Try different window sizes
     disparity_range = (..., ...)  # Determine appropriate disparity range
-    matching_functions = ['SSD', 'SAD', 'normalized_correlation']  # Try different matching functions
-    
+    matching_functions = [
+        "SSD",
+        "SAD",
+        "normalized_correlation",
+    ]  # Try different matching functions
+
     disparity_maps = []
-    
+
     # Generate disparity maps for different settings
     for window_size in window_sizes:
         for matching_function in matching_functions:
-            print(f"Computing disparity map for window_size={window_size}, disparity_range={disparity_range}, matching_function={matching_function}")
+            print(
+                f"Computing disparity map for window_size={window_size}, disparity_range={disparity_range}, matching_function={matching_function}"
+            )
             disparity_map = task1_compute_disparity_map_simple(
-                ref_img, sec_img, 
-                window_size, disparity_range, matching_function)
-            disparity_maps.append((disparity_map, window_size, matching_function, disparity_range))
+                ref_img, sec_img, window_size, disparity_range, matching_function
+            )
+            disparity_maps.append(
+                (disparity_map, window_size, matching_function, disparity_range)
+            )
             dmin, dmax = disparity_range
             visualize_disparity_map(
-                disparity_map, gt_map, 
-                save_path=f"output/task1_{img_name}_{window_size}_{dmin}_{dmax}_{matching_function}.png")
+                disparity_map,
+                gt_map,
+                save_path=f"output/task1_{img_name}_{window_size}_{dmin}_{dmax}_{matching_function}.png",
+            )
     return disparity_maps
 
 
 def task2_compute_depth_map(disparity_map, baseline, focal_length):
-    '''Compute depth map by z = fB / (x - x')
-    Note that a disparity less or equal to zero should be ignored (set to zero) 
-    '''
+    """Compute depth map by z = fB / (x - x')
+    Note that a disparity less or equal to zero should be ignored (set to zero)
+    """
     depth_map = ...
     return depth_map
 
 
 def task2_visualize_pointcloud(
-    ref_img: np.ndarray,        # shape (H, W, 3) 
+    ref_img: np.ndarray,  # shape (H, W, 3)
     disparity_map: np.ndarray,  # shape (H, W)
-    save_path: str = 'output/task2_tsukuba.ply'
+    save_path: str = "output/task2_tsukuba.ply",
 ):
-    '''Visualize 3D pointcloud from disparity map following the steps:
+    """Visualize 3D pointcloud from disparity map following the steps:
     1. Calculate depth map from disparity
     2. Set pointcloud's XY as image's XY and and pointcloud's Z as depth
     3. Set pointcloud's color as ref_img's color
     4. Save pointcloud to ply files for visualizationh. We recommend to open ply file with MeshLab
     5. Adjust the baseline and focal_length for better performance
     6. You may need to cut some outliers for better performance
-    '''
+    """
     baseline = 10
     focal_length = 10
     depth_map = task2_compute_depth_map(disparity_map, baseline, focal_length)
@@ -110,20 +119,19 @@ def task2_visualize_pointcloud(
     # Save pointcloud to ply file
     pointcloud = trimesh.PointCloud(points, colors)
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    pointcloud.export(save_path, file_type='ply')
+    pointcloud.export(save_path, file_type="ply")
 
 
 def task3_compute_disparity_map_dp(ref_img, sec_img):
-    ''' Conduct stereo matching with dynamic programming
-    '''
-    
+    """Conduct stereo matching with dynamic programming"""
+
     disparity_map_dp = ...
 
     return disparity_map_dp
 
 
-def main(tasks): 
-    
+def main(tasks):
+
     # Read images and ground truth disparity maps
     moebius_img1 = cv2.imread("data/moebius1.png")
     moebius_img1_gray = cv2.cvtColor(moebius_img1, cv2.COLOR_BGR2GRAY)
@@ -136,19 +144,23 @@ def main(tasks):
     tsukuba_img2 = cv2.imread("data/tsukuba2.jpg")
     tsukuba_img2_gray = cv2.cvtColor(tsukuba_img2, cv2.COLOR_BGR2GRAY)
     tsukuba_gt = cv2.imread("data/tsukuba_gt.jpg", cv2.IMREAD_GRAYSCALE)
-    
+
     # Task 0: Visualize cv2 Results
-    if '0' in tasks:   
+    if "0" in tasks:
         # Compute disparity maps using cv2
         stereo = cv2.StereoBM.create(numDisparities=64, blockSize=15)
         moebius_disparity_cv2 = stereo.compute(moebius_img1_gray, moebius_img2_gray)
         visualize_disparity_map(moebius_disparity_cv2, moebius_gt)
         tsukuba_disparity_cv2 = stereo.compute(tsukuba_img1_gray, tsukuba_img2_gray)
         visualize_disparity_map(tsukuba_disparity_cv2, tsukuba_gt)
-        
-        if '2' in tasks:
-            print('Running task2 with cv2 results ...')
-            task2_visualize_pointcloud(tsukuba_img1, tsukuba_disparity_cv2, save_path='output/task2_tsukuba_cv2.ply')
+
+        if "2" in tasks:
+            print("Running task2 with cv2 results ...")
+            task2_visualize_pointcloud(
+                tsukuba_img1,
+                tsukuba_disparity_cv2,
+                save_path="output/task2_tsukuba_cv2.ply",
+            )
 
     ######################################################################
     # Note. Running on moebius may take a long time with your own code   #
@@ -156,38 +168,56 @@ def main(tasks):
     ######################################################################
 
     # Task 1: Simple Disparity Algorithm
-    if '1' in tasks:
-        print('Running task1 ...')
-        disparity_maps = task1_simple_disparity(tsukuba_img1_gray, tsukuba_img2_gray, tsukuba_gt, img_name='tsukuba')
-        
+    if "1" in tasks:
+        print("Running task1 ...")
+        disparity_maps = task1_simple_disparity(
+            tsukuba_img1_gray, tsukuba_img2_gray, tsukuba_gt, img_name="tsukuba"
+        )
+
         #####################################################
         # If you want to run on moebius images,             #
         # parallelizing with multiprocessing is recommended #
         #####################################################
         # task1_simple_disparity(moebius_img1_gray, moebius_img2_gray, moebius_gt, img_name='moebius')
-        
-        if '2' in tasks:
-            print('Running task2 with disparity maps from task1 ...')
-            for (disparity_map, window_size, matching_function, disparity_range) in disparity_maps:
+
+        if "2" in tasks:
+            print("Running task2 with disparity maps from task1 ...")
+            for (
+                disparity_map,
+                window_size,
+                matching_function,
+                disparity_range,
+            ) in disparity_maps:
                 dmin, dmax = disparity_range
                 task2_visualize_pointcloud(
-                    tsukuba_img1, disparity_map, 
-                    save_path=f'output/task2_tsukuba_{window_size}_{dmin}_{dmax}_{matching_function}.ply')      
-        
-    # Task 3: Non-local constraints
-    if '3' in tasks:
-        print('----------------- Task 3 -----------------')
-        tsukuba_disparity_dp = task3_compute_disparity_map_dp(tsukuba_img1_gray, tsukuba_img2_gray)
-        visualize_disparity_map(tsukuba_disparity_dp, tsukuba_gt, save_path='output/task3_tsukuba.png')
-        
-        if '2' in tasks:
-            print('Running task2 with disparity maps from task3 ...')
-            task2_visualize_pointcloud(tsukuba_img1, tsukuba_disparity_dp, save_path='output/task2_tsukuba_dp.ply')
+                    tsukuba_img1,
+                    disparity_map,
+                    save_path=f"output/task2_tsukuba_{window_size}_{dmin}_{dmax}_{matching_function}.ply",
+                )
 
-if __name__ == '__main__':
+    # Task 3: Non-local constraints
+    if "3" in tasks:
+        print("----------------- Task 3 -----------------")
+        tsukuba_disparity_dp = task3_compute_disparity_map_dp(
+            tsukuba_img1_gray, tsukuba_img2_gray
+        )
+        visualize_disparity_map(
+            tsukuba_disparity_dp, tsukuba_gt, save_path="output/task3_tsukuba.png"
+        )
+
+        if "2" in tasks:
+            print("Running task2 with disparity maps from task3 ...")
+            task2_visualize_pointcloud(
+                tsukuba_img1,
+                tsukuba_disparity_dp,
+                save_path="output/task2_tsukuba_dp.ply",
+            )
+
+
+if __name__ == "__main__":
     # Set tasks to run
-    parser = argparse.ArgumentParser(description='Homework 4')
-    parser.add_argument('--tasks', type=str, default='0123')
+    parser = argparse.ArgumentParser(description="Homework 4")
+    parser.add_argument("--tasks", type=str, default="0123")
     args = parser.parse_args()
 
     main(args.tasks)
